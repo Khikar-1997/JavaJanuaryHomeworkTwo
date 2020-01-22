5ed8dbf49cdf6028587533dc469e2490a512796f
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-01-21 16:54:55 -0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,22 @@
 package com.company;
 
-import com.company.photo_album.PhotoAlbum;
+import com.company.house.Door;
+import com.company.house.OpenOrCloseHouseDoor;
+import com.company.house.Window;
 
 public class Main {
 
     public static void main(String[] args) {
-        PhotoAlbum photoAlbum = new PhotoAlbum("The nice World");
-        photoAlbum.addPhoto();
-        photoAlbum.giveNameToPhotoByIndex();
-        photoAlbum.printQuantityOfPhothos();
+        OpenOrCloseHouseDoor houseDoor = new OpenOrCloseHouseDoor() {
+            @Override
+            public void lockTheHouseByKey() {
+                System.out.println("The house door is open by key.");
+            }
+        };
+        houseDoor.lockTheHouseByKey();
+        Window window = new Window((short) 5);
+        Door door = new Door(4);
+        window.printQuantityOfWindows();
+        door.printQuantityOfDoors();
     }
 }
